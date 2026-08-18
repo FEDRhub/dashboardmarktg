8654afbdbf1c78485d046a947792d46143766416
FEDRhub <celine.massereau3696@gmail.com>
2026-08-18 17:03:44 +0200

    Create page.js

---
+++ app/digest/page.js
@@ -0,0 +1,9 @@
+import DigestView from '../../components/DigestView';
+
+export const metadata = {
+  title: 'Digest — Veille marketing',
+};
+
+export default function DigestPage() {
+  return <DigestView />;
+}
